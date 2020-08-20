f200e568696d9f5e854538dfdbb5f368449947ba
Hiyokunotori <rui_love@live.com>
2020-08-20 17:57:00 +0800

    first commit

---
+++ mogaixp/XposedBridge/app/src/main/java/de/robv/android/alanxp/callbacks/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Contains the base classes for callbacks.
+ *
+ * <p>For historical reasons, {@link de.robv.android.alanxp.XC_MethodHook} and
+ * {@link de.robv.android.alanxp.XC_MethodReplacement} are directly in the
+ * {@code de.robv.android.alanxp} package.
+ */
+package de.robv.android.alanxp.callbacks;
+
---
+++ mogaixp/XposedInstaller/app/src/main/java/de/robv/android/alanxp/installer/util/RunnableWithParam.java
@@ -0,0 +1,5 @@
+package de.robv.android.alanxp.installer.util;
+
+public interface RunnableWithParam<T> {
+    public void run(T param);
+}
